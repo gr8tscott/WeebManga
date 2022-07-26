43a1f322152f9f656212ff6d2088b95b3a26df9d
MatthewScott <mattwillscott95@gmail.com>
2022-07-26 19:09:38 -0400

    started making form for reviews

--- client/src/components/Reviews.jsx
+++ client/src/components/Reviews.jsx
@@ -20,7 +20,30 @@ const Reviews = (props) => {
 
   return (
     <div className='reviewCard'>
-
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="type">Type of Issue:</label>
+            <select id="type" onChange={handleChange} value={formState.type}>
+            <option value="outage">Service Outage</option>
+            <option value="billing">Billing</option>
+            <option value="cancel">Cancel Service</option>
+            </select>
+            <label htmlFor="subject">Subject:</label>
+            <input
+            type="text"
+            id="subject"
+            onChange={handleChange}
+            value={formState.subject}
+            />
+            <label htmlFor="message">Message</label>
+            <textarea
+            id="message"
+            cols="30"
+            rows="10"
+            onChange={handleChange}
+            value={formState.message}
+            ></textarea>
+            <button type="submit">Submit Review!</button>
+      </form>
         <h1>Current Reviews:</h1>
         {review.map((reviews) => {
             return(
